d3d4f2f5c35bb6b6b3691062c4e25bcf6ccbdd2f
Abhijith Shaji <112998926+abhijeeeth@users.noreply.github.com>
2026-07-17 14:49:40 +0530

    feat: add MapWorld component with embedded Google Maps location iframe

--- src/components/MapWorld.vue
+++ src/components/MapWorld.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="w-full h-full relative min-h-[240px]">
     <iframe
-      src="https://maps.google.com/maps?q=Pottamkulam%20Central%20Building,%20Kalaketty,%20Kanjirapally,%20Kerala%20686508,%20India&t=&z=14&ie=UTF8&iwloc=&output=embed"
+      src="https://maps.google.com/maps?q=Pottamkulam%20Central%20Building,%20Kalaketty,%20Kanjirapally,%20Kerala%20686508,%20India&t=k&z=14&ie=UTF8&iwloc=&output=embed"
       class="w-full h-full border-0 absolute inset-0"
-      allowfullscreen=""
+      allowfullscreen
       loading="lazy"
       referrerpolicy="no-referrer-when-downgrade"
     ></iframe>
